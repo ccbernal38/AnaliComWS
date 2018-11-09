f4e6363cc6d2cb082e8e90d940b74dfb2dc1aa42
Cristian Cruz <criscruz1806@gmail.com>
2018-11-09 15:00:46 -0500

    visiometria sin probar

--- src/co/analicom/ws/controller/PruebaInsert.java
+++ src/co/analicom/ws/controller/PruebaInsert.java
@@ -9,8 +9,6 @@ import co.analicom.ws.hc.modelo.HistoriaVisiometria;
 public class PruebaInsert {
 
 	public static void main(String[] args) {
-		
-		//HistoriaVisiometria historiaVisiometria = new HistoriaVisiometria(, 28, 1018416696, 1094940706, "PERIÓDICO", "", fechaDiligenciamiento, fechaModificacion, impresionDiagnostica, impresionDiagnosticaObservacion, userModifica, impreso, cantImpresiones, estado, pacienteCompatibleLabor, pacienteCompatibleLabor_Observacion, requiereNuevaValoracion, requiereNuevaValoracion_Observacion, requiereRemisionEspecialista, requiereRemisionEspecialista_Observacion, firmaPaciente, firmaMedico)
 		AgudezaVisual agudezaVisual = new AgudezaVisual(10949, "12", "12345", "1094940705", "Prueba",
 				"prueba 2", "prueba" ,"prueba", "impreison diag", 
 				"impresionObserv", "Bernal", "impreso", "12", "prueba", "pacienteCompatible", 
